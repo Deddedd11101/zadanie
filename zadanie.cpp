94c7e9f9e67f9d9f4eb3a2a37becd8176eba95dd
Востриков Антон <93368273+Deddedd11101@users.noreply.github.com>
2021-11-26 15:32:25 +0300

    Add files via upload

---
+++ zadanie.cpp
@@ -0,0 +1,19 @@
+﻿// fefwefewf.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+
+using namespace std;
+typedef struct _SHFILEOPSTRUCTA;
+int main()
+{
+	SHFILEOPSTRUCT sh;
+	ZeroMemory(&sh, sizeof(SHFILEOPSTRUCT));
+	sh.pFrom = L"C:\\Users\\Пользователь\\Desktop\\дщд\\*.tmp*\0";
+	sh.fFlags = FOF_FILESONLY | FOF_NOCONFIRMATION;
+	sh.wFunc = FO_DELETE;
+	SHFileOperation(&sh);
+}
